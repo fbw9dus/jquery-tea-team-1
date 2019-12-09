ca24592eab04cba41318f76bbf291021e9737285
DciAlex <alexgherman68@gmail.com>
2019-12-09 11:56:14 +0100

    v1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Work on your tasks here
+$(document).ready(function(){
+    $('.submit').click(function(){
+        $('.modal').css("display","block");
+    $('.modal-close').click(function(){
+        $('.modal').css("display","none");
+    })
+    })
+})
+
+$(document).ready(function(){
+    $('.teapot').click(function(){
+        $(this).toggleClass("rotate");
+    });
+});
+
+$(".selection__option")
+.mouseover(function(){
+    $(this).find("p").text("sadsdsadsadsad");
+})
+.mouseout(function(){
+    $(this).find("p").text("");
+});
